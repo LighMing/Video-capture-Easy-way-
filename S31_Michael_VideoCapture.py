fa61eeb09a26e9c61a39607e2a490f649d9c6551
LighMing <139311280+LighMing@users.noreply.github.com>
2024-06-11 12:56:34 -0400

    Update_VideoCapture.py

--- S31_Michael_VideoCapture.py
+++ S31_Michael_VideoCapture.py
@@ -9,7 +9,6 @@ out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640,480))
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret==True:
-        # frame = cv2.flip(frame,0)  # 这行代码已被注释掉，不再进行翻转
 
         out.write(frame)
 
